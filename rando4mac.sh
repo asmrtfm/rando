b253c19cc9ca9091e03b013fe98a193a26d5afe1
Zach Pendleton <154548075+asmrtfm@users.noreply.github.com>
2024-05-13 12:45:53 -0400

    Update rando4mac.sh

--- rando4mac.sh
+++ rando4mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rando() {
-  randomNumber() { local -i n=0; for((i=0;i<${#RANDOM};i++)){ n+=${RANDOM:$i:1} ; }; printf '%i' $((n<62?n:n-62)); };
-  local Alnums="$(echo {{a..z},{0..9},{A..Z}} | sed 's|\s*||g')"
-  for((x=0;x<$1;x++)){ printf '%s' "${Alnums:$(randomNumber):1}"; }
+  N() { local -i n=0; for((i=0;i<${#RANDOM};i++)){ n+=${RANDOM:$i:1} ; }; printf '%i' $((n<62?n:n-62)); };
+  local -a Alnums=( {{a..z},{0..9},{A..Z}} )
+  for((x=0;x<$1;x++)){ printf '%s' "${Alnums[$(N)]}"; }
 }
 #rando <Number>
